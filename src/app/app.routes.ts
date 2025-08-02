804c2cfbbe490457207de36980599839913eba0d
ashwathb <ashwathb@examroom.ai>
2025-08-02 23:46:40 +0530

    Added Hero component with responsive design and contact section structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      { path: '', component: HomeComponent },   // default route
+  { path: 'home', component: HomeComponent },
+  // later: { path: 'about', component: AboutComponent },
+  //        { path: 'projects', component: ProjectsComponent },
+  //        { path: 'contact', component: ContactComponent },
+];
